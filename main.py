da8b3474f341f42618cf2dfa5af945f0c88fee99
samkim <ksw593vn@gmail.com>
2025-01-27 23:30:35 +0900

    feat: Add submission history feature

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import os
+import csv
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
+from slack_sdk import WebClient
 from dotenv import load_dotenv
 from datetime import datetime
-import csv
 
 load_dotenv()
 
@@ -19,10 +20,9 @@ def say_hello(message, say):
     say(f"Hi there, <@{user}>!")
 
 
-@app.command("/문의사항 보내기")
+@app.command("/문의사항제출")
 def handle_submit_command(ack, body, client):
     ack()
-    print(body)
     client.views_open(
         trigger_id=body["trigger_id"],
         view={
@@ -109,6 +109,7 @@ def handle_view_submission_events(ack, body, client, logger):
     ack()
 
     # 저장할 데이터
+    user_id = body["user"]["id"]
     user_info = client.users_info(user=body["user"]["id"])
     user_name = body["view"]["state"]["values"]["user_block_id"]["user_action_id"][
         "value"
@@ -119,7 +120,6 @@ def handle_view_submission_events(ack, body, client, logger):
     ]
     # 생성일자
     create_at = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    print(create_at)
 
     # data 디렉토리가 없다면 생성
     if not os.path.exists("data"):
@@ -129,12 +129,58 @@ def handle_view_submission_events(ack, body, client, logger):
     with open("data/questions.csv", "a", newline="", encoding="utf-8") as csvfile:
         writer = csv.writer(csvfile)
         if not os.path.getsize("data/questions.csv") > 0:
-            writer.writerow(["user_name", "title", "question", "create_at"])
-        writer.writerow([user_name, title, question, create_at])
+            writer.writerow(["user_id", "user_name", "title", "question", "create_at"])
+        writer.writerow([user_id, user_name, title, question, create_at])
 
     # 제출 후 완료 메세지
-    text = f"*<@{user_info['user']['name']}>님의 `질문`이 제출되었습니다. "
+    text = f"*<@{user_info['user']['name']}>님의 `문의 사항`이 제출되었습니다. "
     client.chat_postMessage(channel=channel_id, text=text)
+
+
+@app.command("/제출내역")
+def handle_submission_history_command(ack, body, client: WebClient):
+    ack()
+    user_id = body["user_id"]
+
+    # 사용자의 DM 채널 ID 가져오기
+    response = client.conversations_open(users=user_id)
+    dm_channel_id = response["channel"]["id"]
+    # 만약에 제출내역 파일이 없다면 "제출내역 파일 없음"이라고 메세지 전송하고 종료
+    if not os.path.exists("data/questions.csv"):
+        client.chat_postMessage(channel=dm_channel_id, text="*제출내역이 없습니다.*")
+        return None
+    # 사용자의 제출내역만 필터링
+    submission_list = []
+
+    with open("data/questions.csv", encoding="utf-8") as csvfile:
+        reader = csv.DictReader(csvfile)
+        fieldnames = reader.fieldnames
+        for row in reader:
+            if row["user_id"] == user_id:
+                submission_list.append(row)
+    print(submission_list, "submission_list")
+    # 만약에 제출내역이 없다면 "제출내역 없음"이라고 메세지를 전송하고 종료
+    if not submission_list:
+        client.chat_postMessage(channel=dm_channel_id, text="*제출내역이 없습니다.*")
+        return None
+    # 사용자의 제출내역을 csv 파일로 임시 저장 후 전송
+    temp_dir = "data/temp"
+    if not os.path.exists(temp_dir):
+        os.makedirs(temp_dir)
+
+    temp_file_path = f"{temp_dir}/{user_id}.csv"
+    with open(temp_file_path, "w", newline="", encoding="utf-8") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames)
+        writer.writeheader()
+        writer.writerows(submission_list)
+
+    client.files_upload_v2(
+        channel=dm_channel_id,
+        file=temp_file_path,
+        initial_comment=f"<@{user_id}>님의 제출내역입니다!",
+    )
+    # 임시로 생성한 csv 파일 삭제
+    os.remove(temp_file_path)
 
 
 # Start your app
